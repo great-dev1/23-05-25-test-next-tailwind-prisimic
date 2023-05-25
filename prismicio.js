2f9b0bc7240574c6aed3360b446f59f4c205e578
unknown <zgmr07213@gmail.com>
2023-05-25 11:01:45 -0700

    initial commit

---
+++ prismicio.js
@@ -0,0 +1,51 @@
+import * as prismic from "@prismicio/client";
+import * as prismicNext from "@prismicio/next";
+
+import sm from "./slicemachine.config.json";
+
+/**
+ * The project's Prismic repository name.
+ */
+export const { repositoryName } = sm;
+
+/**
+ * The project's Prismic Route Resolvers. This list determines a Prismic document's URL.
+ *
+ * @type {prismic.ClientConfig['routes']}
+ */
+const routes = [
+  {
+    type: "page",
+    path: "/:uid",
+  },
+  {
+    type: "page",
+    uid: "home",
+    path: "/",
+  },
+  {
+    type: "settings",
+    path: "/",
+  },
+  {
+    type: "navigation",
+    path: "/",
+  },
+];
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
+ */
+export const createClient = ({ previewData, req, ...config } = {}) => {
+  const client = prismic.createClient(repositoryName, {
+    routes,
+    ...config,
+  });
+
+  prismicNext.enableAutoPreviews({ client, previewData, req });
+
+  return client;
+};
